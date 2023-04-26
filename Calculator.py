0f6683535592a7efd3870e3345e59a4e787f03b7
Shravan H J <125745139+shravanhj@users.noreply.github.com>
2023-04-26 10:00:51 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -20,41 +20,50 @@ def thank_you():
     print("\t\tCode by Shravan H J")
 
 
-print("\t\t\t CALCULATOR")
-print()
-print("1: Addition\t\t\t\t2: Substraction")
-print("3: Multiplication\t\t4:Division")
-print("5: Percentage\t\t\t6:Exit")
-print()
-choice = int(input('Select your choice from above options: '))
-print()
+def main_fun():
+    print("\t\t\t CALCULATOR")
+    print()
+    print("1: Addition\t\t\t\t2: Substraction")
+    print("3: Multiplication\t\t4:Division")
+    print("5: Percentage\t\t\t6:Exit")
+    print()
+    choice = int(input('Select your choice from above options: '))
+    print()
 
-if choice==6:
-    thank_you()
-elif choice==1:
-    Num1 = int(input('Enter First value: '))
-    Num2 = int(input('Enter Second value: '))
-    print(Num1,"+",Num2,"=", for_plus(Num1, Num2))
+    if choice==6:
+        thank_you()
+    elif choice==1:
+        Num1 = int(input('Enter First value: '))
+        Num2 = int(input('Enter Second value: '))
+        print(Num1,"+",Num2,"=", for_plus(Num1, Num2))
+        main_fun()
 
-elif choice == 2:
-    Num1 = int(input('Enter First value: '))
-    Num2 = int(input('Enter Second value: '))
-    print(Num1, "-", Num2, "=", for_substraction(Num1, Num2))
+    elif choice == 2:
+        Num1 = int(input('Enter First value: '))
+        Num2 = int(input('Enter Second value: '))
+        print(Num1, "-", Num2, "=", for_substraction(Num1, Num2))
+        main_fun()
 
-elif choice == 3:
-    Num1 = int(input('Enter First value: '))
-    Num2 = int(input('Enter Second value: '))
-    print(Num1, "*", Num2, "=", for_multiplication(Num1, Num2))
+    elif choice == 3:
+        Num1 = int(input('Enter First value: '))
+        Num2 = int(input('Enter Second value: '))
+        print(Num1, "*", Num2, "=", for_multiplication(Num1, Num2))
+        main_fun()
 
-elif choice == 4:
-    Num1 = int(input('Enter First value: '))
-    Num2 = int(input('Enter Second value: '))
-    print(Num1, "/", Num2, "=", for_Division(Num1, Num2))
+    elif choice == 4:
+        Num1 = int(input('Enter First value: '))
+        Num2 = int(input('Enter Second value: '))
+        print(Num1, "/", Num2, "=", for_Division(Num1, Num2))
+        main_fun()
 
-elif choice == 5:
-    Num1 = int(input('Enter First value: '))
-    Num2 = int(input('Enter Total value: '))
-    print(Num1, "-", Num2, "=",for_percentage(Num1, Num2),"%")
+    elif choice == 5:
+        Num1 = int(input('Enter First value: '))
+        Num2 = int(input('Enter Total value: '))
+        print(Num1, "-", Num2, "=",for_percentage(Num1, Num2),"%")
+        main_fun()
 
-else:
-    print("Please Enter Correct input.!!!!")
+    else:
+        print("Please Enter Correct input.!!!!")
+        main_fun()
+
+main_fun()
